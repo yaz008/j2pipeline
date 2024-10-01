7091d49bd0a7c4a1b437c104a21643cdb57d9ce5
Emelianov Artem <yaz008.yaz008@yandex.ru>
2024-10-01 17:17:05 +0300

    Update types.pipeline::Pipeline

--- j2pipeline/types/pipeline.py
+++ j2pipeline/types/pipeline.py
@@ -1,25 +1,23 @@
 from dataclasses import dataclass, field
 from os import listdir
 from j2pipeline.types.prompt import Prompt
-from typing import Self
+from typing import Callable
 
 @dataclass(slots=True)
-class Pipeline:
+class Pipeline[T]:
     base_path: str
-    __prompts: list[str] = field(init=False)
-    __current: int = field(default=0, init=False)
+    process: dict[str, Callable[[str], str]] = field(default_factory=dict)
+    final_process: Callable[[str], T] = field(default=lambda result: result)
 
     def __post_init__(self) -> None:
-        self.__prompts = [f'{self.base_path}\\{filename}'
-                          for filename
-                          in listdir(path=self.base_path)]
-
-    def __iter__(self) -> Self:
-        return self
+        if '*' not in self.process.keys():
+            self.process.update({ '*': lambda response: response })
     
-    def __next__(self) -> Prompt:
-        if self.__current == len(self.__prompts):
-            raise StopIteration
-        prompt: Prompt = Prompt(path=self.__prompts[self.__current])
-        self.__current += 1
-        return prompt
+    def __call__(self, prompt: str) -> T:
+        result: str = prompt
+        for filename in listdir(path=self.base_path):
+            name: str = filename[:-len('.j2')]
+            prompt: Prompt = Prompt(path=f'{self.base_path}\\{filename}',
+                                    process=self.process.get(name, self.process['*']))
+            result: str = prompt.ask(subs={ 'PROMPT': result })
+        return self.final_process(result)
