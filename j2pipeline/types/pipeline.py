ffcbae3e35f28099778f792cb1f147be0c7b5ff4
Emelianov Artem <yaz008.yaz008@yandex.ru>
2024-10-01 18:17:13 +0300

    Add reorder to types.pipeline::Pipeline

--- j2pipeline/types/pipeline.py
+++ j2pipeline/types/pipeline.py
@@ -6,18 +6,23 @@ from typing import Callable
 @dataclass(slots=True)
 class Pipeline[T]:
     base_path: str
+    reorder: Callable[[list[str]], list[str]] = field(default=lambda prompts: prompts)
     process: dict[str, Callable[[str], str]] = field(default_factory=dict)
     final_process: Callable[[str], T] = field(default=lambda result: result)
+    extension: str = field(default='j2')
+    __names: list[str] = field(init=False)
 
     def __post_init__(self) -> None:
+        self.__names = self.reorder([name[:-len(f'.{self.extension}')]
+                                     for name
+                                     in listdir(path=self.base_path)])
         if '*' not in self.process.keys():
             self.process.update({ '*': lambda response: response })
     
     def __call__(self, prompt: str) -> T:
         result: str = prompt
-        for filename in listdir(path=self.base_path):
-            name: str = filename[:-len('.j2')]
-            prompt: Prompt = Prompt(path=f'{self.base_path}\\{filename}',
+        for name in self.__names:
+            prompt: Prompt = Prompt(path=f'{self.base_path}\\{name}.{self.extension}',
                                     process=self.process.get(name, self.process['*']))
             result: str = prompt(PROMPT=result)
         return self.final_process(result)
