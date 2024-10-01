3151a27294565ac0acdf7a5e6258ed76c7962c7f
Emelianov Artem <yaz008.yaz008@yandex.ru>
2024-10-01 21:16:57 +0300

    Update tcp

--- j2pipeline/tcp/manager.py
+++ j2pipeline/tcp/manager.py
@@ -4,16 +4,13 @@ from typing import Generator, Callable
 from j2pipeline.tcp.client import Client
 
 @contextmanager
-def client(on_receive: Callable[[str], None],
-           timeout: float | None = None,
-           host: str = 'localhost',
+def client(host: str = 'localhost',
            port: int = 50027) -> Generator[Client, None, None]:
     socket: Socket = Socket(family=AddressFamily.AF_INET,
                             type=SocketKind.SOCK_STREAM)
     socket.connect((host, port))
-    client: Client = Client(socket=socket, on_receive=on_receive)
+    client: Client = Client(socket=socket)
     try:
         yield client
     finally:
-        client.join(timeout=timeout)
         client.close()
--- j2pipeline/types/prompt.py
+++ j2pipeline/types/prompt.py
@@ -8,22 +8,10 @@ class Prompt[T]:
     path: str
     process: Callable[[str], T] = field(default=lambda response: response)
     auto_upper: bool = field(default=True)
-    __callback: Callable[[str], NoReturn] = field(init=False)
-    __response: str | None = field(default=None, init=False)
-
-    def __post_init__(self) -> None:
-        def callback(response: str) -> NoReturn:
-            self.__response = response
-            exit(code=0)
-        self.__callback = callback
 
     def __call__(self, **subs: str) -> T:
-        self.__response = None
         if self.auto_upper:
             subs = { key.upper(): value for key, value in subs.items() }
         prompt: str = assemble(path=self.path, subs=subs)
-        with client(on_receive=self.__callback) as clt:
-            clt.send(prompt)
-        while True:
-            if self.__response is not None:
-                return self.process(self.__response)
+        with client() as clt:
+            return clt.send(prompt)
